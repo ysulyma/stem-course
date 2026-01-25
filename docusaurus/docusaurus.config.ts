69db5c9ebb9abbc48da12c892fd2c06bb2c9c17f
Yuri Sulyma <yuri@liqvidjs.org>
2026-01-25 23:07:55 +0100

    moar stuff

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -5,27 +5,15 @@ import { themes as prismThemes } from "prism-react-renderer";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-	title: "JavaScript for STEM",
-	tagline: "Dinosaurs are cool",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/",
 	favicon: "img/favicon.ico",
 
 	// Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
 	future: {
 		v4: true, // Improve compatibility with the upcoming Docusaurus v4
 	},
-
-	// Set the production url of your site here
-	url: "https://your-docusaurus-site.example.com",
-	// Set the /<baseUrl>/ pathname under which your site is served
-	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
-
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
-
-	onBrokenLinks: "throw",
 
 	// Even if you don't use internationalization, you can use this field to set
 	// useful metadata like html lang. For example, if your site is Chinese, you
@@ -35,31 +23,50 @@ const config: Config = {
 		locales: ["en", "fr"],
 	},
 
+	onBrokenLinks: "throw",
+
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "facebook", // Usually your GitHub org/user name.
+
+	plugins: [
+		[
+			"@docusaurus/plugin-ideal-image",
+			{
+				disableInDev: false,
+				max: 1030, // max resized image's size.
+				min: 640, // min resized image's size. if original is lower, use that size.
+				quality: 70,
+				steps: 2, // the max number of images generated between min and max (inclusive)
+			},
+		],
+	],
+
 	presets: [
 		[
 			"classic",
 			{
-				docs: {
-					sidebarPath: "./sidebars.ts",
+				blog: {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
 						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-				},
-				blog: {
-					showReadingTime: true,
 					feedOptions: {
 						type: ["rss", "atom"],
 						xslt: true,
 					},
+					onInlineAuthors: "warn",
+					// Useful options to enforce blogging best practices
+					onInlineTags: "warn",
+					onUntruncatedBlogPosts: "warn",
+					showReadingTime: true,
+				},
+				docs: {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
 						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-					// Useful options to enforce blogging best practices
-					onInlineTags: "warn",
-					onInlineAuthors: "warn",
-					onUntruncatedBlogPosts: "warn",
+					sidebarPath: "./sidebars.ts",
 				},
 				theme: {
 					customCss: "./src/css/custom.css",
@@ -67,42 +74,110 @@ const config: Config = {
 			} satisfies Preset.Options,
 		],
 	],
+	projectName: "docusaurus", // Usually your repo name.
+	tagline: "Dinosaurs are cool",
 
 	themeConfig: {
-		// Replace with your project's social card
-		image: "img/docusaurus-social-card.jpg",
+		// algolia: {
+		// 	// Public API key: it is safe to commit it
+		// 	apiKey: "YOUR_SEARCH_API_KEY",
+		// 	// The application ID provided by Algolia
+		// 	appId: "YOUR_APP_ID",
+		//
+		// 	// Optional: whether you want to use the new Ask AI feature (undefined by default)
+		// 	askAi: "YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID",
+		//
+		// 	// Optional: see doc section below
+		// 	contextualSearch: true,
+		//
+		// 	// Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+		// 	externalUrlRegex: "external\\.com|domain\\.com",
+		//
+		// 	indexName: "YOUR_INDEX_NAME",
+		//
+		// 	// Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+		// 	insights: false,
+		//
+		// 	// Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+		// 	replaceSearchResultPathname: {
+		// 		from: "/docs/", // or as RegExp: /\/docs\//
+		// 		to: "/",
+		// 	},
+		//
+		// 	// Optional: path for search page that enabled by default (`false` to disable it)
+		// 	searchPagePath: "search",
+		//
+		// 	// Optional: Algolia search parameters
+		// 	searchParameters: {},
+		//
+		// 	//... other Algolia params
+		// },
 		colorMode: {
 			respectPrefersColorScheme: true,
 		},
+		footer: {
+			copyright: `Copyright © ${new Date().getFullYear()} Yuri Sulyma. Built with Docusaurus.`,
+			links: [
+				// {
+				// 	title: "Docs",
+				// 	items: [
+				// 		{
+				// 			label: "Tutorial",
+				// 			to: "/docs/intro",
+				// 		},
+				// 	],
+				// },
+				{
+					items: [
+						{
+							href: "https://discord.gg/u8Qab99zHx",
+							label: "Discord",
+						},
+					],
+					title: "Community",
+				},
+				{
+					items: [
+						// {
+						// 	label: "Blog",
+						// 	to: "/blog",
+						// },
+						{
+							href: "https://github.com/facebook/docusaurus",
+							label: "GitHub",
+						},
+					],
+					title: "More",
+				},
+			],
+			style: "dark",
+		},
+		// Replace with your project's social card
+		image: "img/docusaurus-social-card.jpg",
 		navbar: {
-			title: "JS × STEM",
-			logo: {
-				alt: "My Site Logo",
-				src: "img/logo.svg",
-			},
 			items: [
 				{
-					type: "docSidebar",
-					sidebarId: "tutorialSidebar",
-					position: "left",
 					label: "🍦 Vanilla",
-				},
-				{
-					type: "docSidebar",
-					sidebarId: "reactSidebar",
 					position: "left",
-					label: "🪩 Framework",
-				},
-				{
+					sidebarId: "tutorialSidebar",
 					type: "docSidebar",
-					sidebarId: "liqvidSidebar",
-					position: "left",
-					label: "🤯 Video",
 				},
-				{ to: "showcase", label: "🖼 Showcase", position: "right" },
+				// {
+				// 	label: "🪩 Framework",
+				// 	position: "left",
+				// 	sidebarId: "reactSidebar",
+				// 	type: "docSidebar",
+				// },
+				// {
+				// 	label: "🤯 Video",
+				// 	position: "left",
+				// 	sidebarId: "liqvidSidebar",
+				// 	type: "docSidebar",
+				// },
+				{ label: "🖼 Showcase", position: "right", to: "showcase" },
 				{
-					type: "localeDropdown",
 					position: "right",
+					type: "localeDropdown",
 				},
 				{
 					href: "https://github.com/facebook/docusaurus",
@@ -110,53 +185,21 @@ const config: Config = {
 					position: "right",
 				},
 			],
-		},
-		footer: {
-			style: "dark",
-			links: [
-				{
-					title: "Docs",
-					items: [
-						{
-							label: "Tutorial",
-							to: "/docs/intro",
-						},
-					],
-				},
-				{
-					title: "Community",
-					items: [
-						{
-							label: "Stack Overflow",
-							href: "https://stackoverflow.com/questions/tagged/docusaurus",
-						},
-						{
-							label: "Discord",
-							href: "https://discord.gg/u8Qab99zHx",
-						},
-					],
-				},
-				{
-					title: "More",
-					items: [
-						// {
-						// 	label: "Blog",
-						// 	to: "/blog",
-						// },
-						{
-							label: "GitHub",
-							href: "https://github.com/facebook/docusaurus",
-						},
-					],
-				},
-			],
-			copyright: `Copyright © ${new Date().getFullYear()} Yuri Sulyma. Built with Docusaurus.`,
+			logo: {
+				alt: "My Site Logo",
+				src: "img/logo.svg",
+			},
+			title: "JS × STEM",
 		},
 		prism: {
-			theme: prismThemes.github,
 			darkTheme: prismThemes.dracula,
+			theme: prismThemes.github,
 		},
 	} satisfies Preset.ThemeConfig,
+	title: "JavaScript for STEM",
+
+	// Set the production url of your site here
+	url: "https://your-docusaurus-site.example.com",
 };
 
 export default config;
