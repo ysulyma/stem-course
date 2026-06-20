4edd03d37305e50c9056955d521053978cc6bfa2
Yuri Sulyma <yuri.sulyma@protonmail.com>
2026-06-20 12:24:00 -0600

    more stuff

---
+++ docusaurus/src/theme/NavbarItem/ComponentTypes.ts
@@ -0,0 +1,8 @@
+import ComponentTypes from "@theme-original/NavbarItem/ComponentTypes";
+
+import { ToggleTextVideo } from "../../components/ToggleTextVideo";
+
+export default {
+	...ComponentTypes,
+	"custom-ToggleTextVideo": ToggleTextVideo,
+};
